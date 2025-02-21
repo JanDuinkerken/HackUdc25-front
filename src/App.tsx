0c46ea12d5cad71a81a18a70aadf24e50c16f1ab
Jan Duinkerken Rodríguez <jan.duinkerken.rodriguez@gmail.com>
2025-02-21 23:00:41 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+import { Home } from './Home';
+
+export default function App() {
+  return <MantineProvider>
+    {
+      <Home />
+    }
+  </MantineProvider>;
+}
+
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import {
+  IconLogout,
+  IconNotebook,
+  IconRobotFace,
+  IconTarget,
+} from '@tabler/icons-react';
+import classes from './Navbar.module.css';
+
+const data = [
+  { link: '', label: 'ChatBot', icon: IconRobotFace },
+  { link: '', label: 'Mi diario emocional', icon: IconNotebook },
+  { link: '', label: 'Mi perfil emocional', icon: IconTarget },
+];
+
+export function Navbar() {
+  const [active, setActive] = useState('Billing');
+
+  const links = data.map((item) => (
+    <a
+      className={classes.link}
+      data-active={item.label === active || undefined}
+      href={item.link}
+      key={item.label}
+      onClick={(event) => {
+        event.preventDefault();
+        setActive(item.label);
+      }}
+    >
+      <item.icon className={classes.linkIcon} stroke={1.5} />
+      <span>{item.label}</span>
+    </a>
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.navbarMain}>
+        {links}
+      </div>
+
+      <div className={classes.footer}>
+        <a href="#" className={classes.link} onClick={(event) => event.preventDefault()}>
+          <IconLogout className={classes.linkIcon} stroke={1.5} />
+          <span>Logout</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
