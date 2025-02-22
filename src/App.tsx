e423cdc5c1184a38e31d44bb23d99c52fa3949d3
Jan Duinkerken Rodríguez <jan.duinkerken.rodriguez@gmail.com>
2025-02-22 02:14:38 +0100

    feat: basic profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
+import '@mantine/charts/styles.css';
 
 import { MantineProvider } from '@mantine/core';
 import { Home } from './Home';
 
 export default function App() {
-  return <MantineProvider>
-    {
-      <Home />
-    }
-  </MantineProvider>;
+  return (
+    <MantineProvider>
+      {
+        <Home />
+      }
+    </MantineProvider>
+  );
 }
 
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -1,7 +1,76 @@
+import { RadarChart } from "@mantine/charts";
+import { Center, Container, Flex, Group, List, ListItem, Stack, Text, Title } from "@mantine/core";
+import { useState } from "react";
+
 export const Profile = () => {
+    const [data] = useState([
+        {
+            emotion: 'Felicidad',
+            score: 1
+        },
+        {
+            emotion: 'Tristeza',
+            score: 2
+        },
+        {
+            emotion: 'Enojo',
+            score: 3
+        },
+        {
+            emotion: 'Miedo',
+            score: 2
+        },
+        {
+            emotion: 'Sorpresa',
+            score: 3
+        }
+    ])
     return (
-        <div>
-            <h1>Profile</h1>
-        </div>
+        <Stack w={"80vw"} h={"85vh"}>
+            <Container>
+                <Center>
+                    <Title>Mi perfil emocional</Title>
+                </Center>
+            </Container>
+            <RadarChart
+                h={"50vh"}
+                data={data}
+                dataKey="emotion"
+                withPolarRadiusAxis
+                series={[{ name: 'score', color: 'blue.4', opacity: 0.2 }]}
+            />
+            <List>
+                <List.Item>
+                    <Group>
+                        <Text fw={"bold"} size="lg">Neuroticismo:</Text>
+                        <Text>Descriptción del estado emocional</Text>
+                    </Group>
+                </List.Item>
+                <List.Item>
+                    <Group>
+                        <Text fw={"bold"} size="lg">Apertura a la experiencia:</Text>
+                        <Text>Descriptción del estado emocional</Text>
+                    </Group>
+                </List.Item>
+                <List.Item>
+                    <Group>
+                        <Text fw={"bold"} size="lg">Responsabilidad:</Text>
+                        <Text>Descriptción del estado emocional</Text>
+                    </Group>
+                </List.Item>
+                <List.Item>
+                    <Group>
+                        <Text fw={"bold"} size="lg">Extroversión:</Text>
+                        <Text>Descriptción del estado emocional</Text>
+                    </Group>
+                </List.Item>
+                <List.Item>
+                    <Group>
+                        <Text fw={"bold"} size="lg">Amabilidad:</Text>
+                        <Text>Descriptción del estado emocional</Text>
+                    </Group>
+                </List.Item>
+            </List>
+        </Stack>
     );
 }
