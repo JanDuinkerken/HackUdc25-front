378cfd87285afe96d4e37215c52a7a6f2ed19456
Jan Duinkerken Rodríguez <jan.duinkerken.rodriguez@gmail.com>
2025-02-22 03:41:11 +0100

    feat: save and display sent messages

--- src/pages/chatbot/Chatbot.tsx
+++ src/pages/chatbot/Chatbot.tsx
@@ -1,25 +1,59 @@
-import { Center, Container, Group, Input, ScrollArea, Stack, Text, Title } from "@mantine/core";
-import { IconRobotFace  , IconSend2 } from "@tabler/icons-react";
+import { Center, Container, Group, Paper, ScrollArea, Stack, Text, TextInput, Title } from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { IconRobotFace, IconSend2 } from "@tabler/icons-react";
+import { useState } from "react";
 
 export const Chatbot = () => {
+    const [messages, setMessages] = useState<string[]>([]);
+    const form = useForm({
+        initialValues: {
+            message: ""
+        }
+    });
+
+    const sendMessage = (message: string) => {
+        setMessages([...messages, message]);
+        form.reset();
+        console.log(messages);
+    }
+
     return (
         <Stack w={"80vw"} h={"85vh"}>
             <Container h={"80vh"}>
-                <ScrollArea h={"100%"}>
+                <ScrollArea h={"100%"} w={"60vw"}>
                     <Center>
                         <Group>
                             <IconRobotFace size={"5vh"} />
                             <Title>ChatBot</Title>
                         </Group>
                     </Center>
-                    <Text c={"gray"}>
-                        ¡Hola! Soy un chatbot, ¿en qué puedo ayudarte?
-                    </Text>
+                    <Center>
+                        <Text c={"gray"}>
+                            ¡Hola! Soy un chatbot, ¿en qué puedo ayudarte?
+                        </Text>
+                    </Center>
+                    {messages.map((message, index) => (
+                        <Paper key={index}  p={"sm"} m={"sm"} radius={"xl"} w={"40vw"} bg={"gray.1"}>
+                            <Text>
+                                {message}
+                            </Text>
+                        </Paper>
+                    ))}
                 </ScrollArea>
             </Container>
             <Container h={"5vh"}>
                 <Group>
-                    <Input radius={"lg"} opacity={"100%"} w={"60vw"} rightSection={<IconSend2/>} placeholder="¿En que podemos ayudarte?" />
+                    <form onSubmit={form.onSubmit((e) => { sendMessage(e.message) })}>
+                        <TextInput
+                            value={form.values.message}
+                            onChange={(e) => form.setFieldValue('message', e.currentTarget.value)}
+                            radius={"lg"}
+                            opacity={"100%"}
+                            w={"60vw"}
+                            rightSection={<IconSend2 />}
+                            placeholder="¿En que podemos ayudarte?"
+                        />
+                    </form>
                 </Group>
             </Container>
         </Stack>
