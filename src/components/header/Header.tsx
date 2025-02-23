cd0b89ad7d51346c85fa7f288673fc055c1383f1
Jan Duinkerken Rodríguez <jan.duinkerken.rodriguez@gmail.com>
2025-02-23 05:54:17 +0100

    feat: add new logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import { Group, Image, Title } from '@mantine/core';
+import { Group, Image } from '@mantine/core';
 import classes from './Header.module.css';
 
 export function Header() {
@@ -6,9 +6,9 @@ export function Header() {
     <header className={classes.header}>
       <div className={classes.inner}>
         <Group h={"100%"}>
-          <Image src={'logo.png'} alt="Logo" mt={"1vh"} height={"60vh"} />
-          <Title size={"3rem"} mt={"1vh"}>KeleaCare</Title>
-          <Group justify={"end"} w={"81.65vw"} m={0} p={0} h={"100%"}>
+          <Image src={'logo.png'} alt="Logo" height={"60vh"} />
+          <Image src={'ofelia.png'} alt="Title" h={"7vh"} />
+          <Group justify={"end"} w={"82.58vw"} m={0} p={0} h={"100%"}>
             <Image src={'palette.png'} alt="Navbar color gradient" h={"7.9vh"} />
           </Group>
         </Group>
